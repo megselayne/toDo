dad8ec60fc467f7560b5bb2a003dde0cd8598d15
Megan Lee <megselayne@gmail.com>
2020-08-08 19:14:15 -0400

    db update

--- db/migrations/202008081002_update_to_do_table.sql
+++ db/migrations/202008081002_update_to_do_table.sql
@@ -1,8 +1,3 @@
 --left out status column in initial migration
 ALTER TABLE to_dos
 ADD COLUMN status VARCHAR(255);
-
---adding values for columns
-UPDATE to_dos
-SET status = 'todo'
-WHERE id in(1,2);
